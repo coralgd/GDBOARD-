ad252ce9a18e2279789e29d09a5f3f8a05464ff1
coralgd <fufianz@gmail.com>
2026-01-04 14:59:26 +0300

    Update board.js

--- board.js
+++ board.js
@@ -6,7 +6,9 @@ auth.onAuthStateChanged(user => {
     return;
   }
 
+  // Показываем только verified пользователей
   db.collection("users")
+    .where("situation", "==", "verified")
     .orderBy("points", "desc")
     .onSnapshot(snapshot => {
       boardTable.innerHTML = "";
@@ -14,7 +16,7 @@ auth.onAuthStateChanged(user => {
       snapshot.forEach(doc => {
         const data = doc.data();
         const tr = document.createElement("tr");
-        tr.innerHTML = `<td>${rank}</td><td>${data.nick || data.email}</td><td>${data.points}</td>`;
+        tr.innerHTML = `<td>${rank}</td><td>${data.nick}</td><td>${data.points}</td>`;
         boardTable.appendChild(tr);
         rank++;
       });
