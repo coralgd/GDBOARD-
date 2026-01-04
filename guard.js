bdb250093f9c2e69a67e9915a48a8c3c84ccb016
coralgd <fufianz@gmail.com>
2026-01-04 17:36:05 +0300

    Update guard.js

--- guard.js
+++ guard.js
@@ -1,19 +1,17 @@
 // guard.js
-import { auth, db } from "./firebase.js";
+import { auth, db } from "./firebase-config.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 onAuthStateChanged(auth, async (user) => {
   if (!user) {
-    window.location.href = "index.html";
+    location.href = "index.html";
     return;
   }
 
-  const ref = doc(db, "users", user.uid);
-  const snap = await getDoc(ref);
-
+  const snap = await getDoc(doc(db, "users", user.uid));
   if (!snap.exists()) {
-    window.location.href = "index.html";
+    location.href = "index.html";
     return;
   }
 
@@ -25,7 +23,6 @@ onAuthStateChanged(auth, async (user) => {
   }
 
   if (data.situation !== "verified") {
-    window.location.href = "nickname.html";
-    return;
+    location.href = "account.html";
   }
 });
