488dbac52b82bb32dfc67a1017ebf0d5b612da14
coralgd <fufianz@gmail.com>
2026-01-04 15:18:06 +0300

    Update board.js

--- board.js
+++ board.js
@@ -1,21 +1,25 @@
 const boardTable = document.getElementById("boardTable");
 
-auth.onAuthStateChanged(user => {
+auth.onAuthStateChanged(async (user) => {
   if (!user) return;
 
-  // Отображаем только verified пользователей
-  db.collection("users")
-    .where("situation", "==", "verified")
-    .orderBy("points", "desc")
-    .onSnapshot(snapshot => {
-      boardTable.innerHTML = "";
-      let rank = 1;
-      snapshot.forEach(doc => {
-        const data = doc.data();
-        const tr = document.createElement("tr");
-        tr.innerHTML = `<td>${rank}</td><td>${data.nick}</td><td>${data.points}</td>`;
-        boardTable.appendChild(tr);
-        rank++;
-      });
+  try {
+    const snapshot = await db.collection("users")
+      .where("situation", "==", "verified")
+      .orderBy("points", "desc")
+      .get();
+
+    boardTable.innerHTML = "";
+    let rank = 1;
+    snapshot.forEach(doc => {
+      const data = doc.data();
+      const tr = document.createElement("tr");
+      tr.innerHTML = `<td>${rank}</td><td>${data.nick}</td><td>${data.points}</td>`;
+      boardTable.appendChild(tr);
+      rank++;
     });
+
+  } catch (err) {
+    console.error(err);
+  }
 });
