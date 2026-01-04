f23b9873285e91595a8b93ff317e74ee82886c9d
coralgd <fufianz@gmail.com>
2026-01-04 14:57:36 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,3 @@
-// firebase-config.js
-// Подключаем Firebase SDK
 const firebaseConfig = {
   apiKey: "AIzaSyBs8vPfGYlPd-5C69h3SBCs-93gR_iolvs",
   authDomain: "gdboard-coral.firebaseapp.com",
@@ -9,9 +7,7 @@ const firebaseConfig = {
   appId: "1:730451840482:web:64de71b9b3963e421ebdbb"
 };
 
-// Инициализация Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Сохраняем ссылки на сервисы
 const auth = firebase.auth();
 const db = firebase.firestore();
