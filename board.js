2cf4702dda94ec005e1c720d8850637105ee7d6b
coralgd <fufianz@gmail.com>
2026-01-04 14:22:59 +0300

    Update board.js

--- board.js
+++ board.js
@@ -3,20 +3,21 @@ const boardTable = document.getElementById("boardTable");
 auth.onAuthStateChanged(user => {
   if (!user) {
     window.location.href = "index.html";
-  } else {
-    db.collection("users")
-      .where("status", "==", "approved")
-      .orderBy("points", "desc")
-      .onSnapshot(snapshot => {
-        boardTable.innerHTML = "";
-        let rank = 1;
-        snapshot.forEach(doc => {
-          const data = doc.data();
-          const tr = document.createElement("tr");
-          tr.innerHTML = `<td>${rank}</td><td>${data.nickname}</td><td>${data.points}</td>`;
-          boardTable.appendChild(tr);
-          rank++;
-        });
-      });
+    return;
   }
+
+  db.collection("users")
+    .where("situation", "==", "verified")
+    .orderBy("points", "desc")
+    .onSnapshot(snapshot => {
+      boardTable.innerHTML = "";
+      let rank = 1;
+      snapshot.forEach(doc => {
+        const data = doc.data();
+        const tr = document.createElement("tr");
+        tr.innerHTML = `<td>${rank}</td><td>${data.nick}</td><td>${data.points}</td>`;
+        boardTable.appendChild(tr);
+        rank++;
+      });
+    });
 });
